5c172b9f72ae6ac1d80f5a2a7c4f659ba3db1962
VukasinVujic <vukasinvujic021@gmail.com>
2020-10-21 13:59:45 +0200

    Adding documents in collection

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Node_beginners_4_mongodb_task-manager
+
+import, declear MongoClient,
+
+add url to db server,
+
+give name to database(databaseName)
+
+when conect to db start callback function
+inside callback function add the name of collection(table) "users"
+
+add in that table one object that is like a row in table
+add in that table many object that is like a row in table
--- mongodb.js
+++ mongodb.js
@@ -4,21 +4,66 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = "mongodb://127.0.0.1:27017";
 
 const databaseName = "task-manger";
+// const databaseName2 = "task-manger2";
 
 MongoClient.connect(
   connectionURL,
-  { useNewUrlParser: true },
+  { useUnifiedTopology: true, useNewUrlParser: true },
   (error, client) => {
     if (error) {
       return console.log("Unable to conect ");
     }
 
     const db = client.db(databaseName);
+    // const db2 = client.db(databaseName2);
+    // //insert one user
+    // db.collection("users").insertOne(
+    //   {
+    //     // insertone will be called as async function
+    //     name: "Vukasin",
+    //     age: 33,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert");
+    //     }
 
-    db.collection("users").insertOne({
-      // insert one will be called as async function
-      name: "Vukasin",
-      age: 33,
-    });
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       name: "Jen",
+    //       age: 28,
+    //     },
+    //     {
+    //       name: "Gunter",
+    //       age: 27,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert documents(objects)");
+    //     }
+
+    //     console.log(result.ops);
+    //   }
+    // );
+
+    db.collection("user2").insertMany(
+      [
+        { description: "beautiful", complited: true },
+        { description: "ugly", complited: false },
+        { description: "strong", complited: true },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert documents");
+        }
+        console.log(result.ops);
+      }
+    );
   }
 );
