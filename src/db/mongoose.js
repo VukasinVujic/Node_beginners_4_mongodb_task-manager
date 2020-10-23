ea436a7566da36d7222c9c27706e18caa84b7989
VukasinVujic <vukasinvujic021@gmail.com>
2020-10-23 18:31:53 +0200

    challenge validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -26,6 +26,23 @@ const User = mongoose.model("User", {
       }
     },
   },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Value can not containe word 'password'");
+      }
+    },
+    // validate(value) {
+    //   if (!validator.isByteLength(value, 0, 6)) {
+    //     throw new Error("Value can not be bigger then 6 characters");
+    //   }
+    // },
+  },
+
   age: {
     type: Number,
     default: 0,
@@ -39,37 +56,41 @@ const User = mongoose.model("User", {
 });
 
 // create an instance of the model
-const me = new User({
-  name: "   Mike    ",
-  email: "lalala@MEAD.IO    ",
-});
-
-// to save instance in db, returns promise
-me.save()
-  .then(() => {
-    console.log(me);
-  })
-  .catch((error) => {
-    console.log("error: " + error);
-  });
-
-// const Task = mongoose.model("Task", {
-//   descrition: {
-//     type: String,
-//
-//   },
-//   complited: {
-//     type: Boolean,
-//   },
+// const me = new User({
+//   name: "   Mike    ",
+//   email: "lalala@MEAD.IO    ",
+//   password: "passwordaaa",
 // });
 
-// const newTask = new Task({ descrition: "clean room", complited: false });
-
-// newTask
-//   .save()
+// to save instance in db, returns promise
+// me.save()
 //   .then(() => {
-//     console.log(newTask);
+//     console.log(me);
 //   })
 //   .catch((error) => {
-//     console.log("Error: " + error);
+//     console.log("error: " + error);
 //   });
+
+const Task = mongoose.model("Task", {
+  descrition: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  complited: {
+    type: Boolean,
+    required: false,
+    default: false,
+  },
+});
+
+const newTask = new Task({ descrition: "    clean room    ", complited: true });
+// complited: false
+newTask
+  .save()
+  .then(() => {
+    console.log(newTask);
+  })
+  .catch((error) => {
+    console.log("Error: " + error);
+  });
